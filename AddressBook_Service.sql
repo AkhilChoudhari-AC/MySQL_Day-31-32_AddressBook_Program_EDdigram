411ea58f8b5968fcc5db60c5752f6978ed2fad83
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-08-13 07:51:05 +0530

    ADDED: Creating tables like first name, last name,address,zip code, email

--- AddressBook_Service.sql
+++ AddressBook_Service.sql
@@ -1,4 +1,9 @@
+#UC1
 create database AD_BOOK_SERVICE;
 show databases;
 show databases;
 use AD_BOOK_SERVICE;
+
+#UC2
+create table adbook(id int unsigned not null auto_increment,fname varchar(15) not null,lname varchar(10) not null,city varchar(50) not null,state varchar(50) not null,zip int not null,phone long not null,email varchar(100) not null,primary key (id));
+select * from adbook;
