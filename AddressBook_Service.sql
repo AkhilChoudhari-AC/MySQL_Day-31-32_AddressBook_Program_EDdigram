d3465af5f5301aa3e0e243b7b7e9ef78277b05c8
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-08-13 10:57:43 +0530

    ADDED: ER diagram of Address book services

--- AddressBook_Service.sql
+++ AddressBook_Service.sql
@@ -44,3 +44,28 @@ SELECT COUNT(type) AS Number_Of_Contact_Person FROM adbook;
 #UC11
 insert into adbook(fname, lname, city, state, zip, phone, email) values('Pinky', 'sharma', 'Sage', 'MP', 414501, 9999888777, 'pinky@gmail.com' );
 update  adbook set type='Family' where id =5;
+
+#UC12
+CREATE TABLE adbbok1 (id int not null auto_increment primary key,firstname varchar(15) NOT NULL, lastname varchar(10) NOT NULL,city varchar(50) NOT NULL,state varchar(50) NOT NULL,zip int(6) NOT NULL ,phone varchar(10) NOT NULL,email varchar(100) NOT NULL UNIQUE);
+desc adbbok1;
+
+create table adbook2(id int not null auto_increment primary key,name varchar (20) not null,ab_id int ,foreign key (ab_id)references adbbok1(id)  );
+desc adbook2;
+create table adbook3(id int not null primary key,type varchar (20));
+desc adbook3;
+create table adbook4(a_id int not null,t_id int not null,foreign key(a_id) references adbbok1(id),foreign key(t_id) references adbook3(id),primary key(a_id,t_id));
+use AD_BOOK_SERVICE;
+INSERT INTO adbbok1 VALUES
+(1,'monu', 'gaik', 'Amravati', 'Maharastra',444806, 9168112901, 'mbg@gmail.com'),
+(2,'sonu', 'wad', 'nagpur', 'Maharashtra', 411402, 8138975257, 'mbg1@gmail.com'),
+(3,'swapnil', 'gaik', 'dighori', 'Maharashtra', 7125487, 8149454587, 'mbg2@gmail.com'),
+(4,'mohit', 'wad', 'chitnispura', 'Maharastra', 414501, 9604315270, 'mbg3@gmail.com');
+insert into adbook2 values (1,'AdddressBook1', 2); 
+insert into adbook3 values(1,'family');
+insert into adbook3 values(2,'friends');
+insert into adbook3 values(3,'profession');
+insert into adbook4(a_id,t_id)values(1,1);
+select * from adbbok1;
+select * from adbook2;
+select * from adbook3;
+select * from adbook4;
